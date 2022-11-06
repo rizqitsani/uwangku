47a3c45adc4fa76451bba6701e67ed97f571e732
rizqitsani <rizqitsani@gmail.com>
2022-11-06 20:44:02 +0700

    feat: init nprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import { AppProps } from 'next/app';
+import { Router } from 'next/router';
+import nProgress from 'nprogress';
 
 import '@/styles/globals.css';
+import '@/styles/nprogress.css';
+
+Router.events.on('routeChangeStart', nProgress.start);
+Router.events.on('routeChangeError', nProgress.done);
+Router.events.on('routeChangeComplete', nProgress.done);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
