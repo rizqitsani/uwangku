b68e5bff54d48a5bdb85b87ced2ec44644d4cd8b
rizqitsani <rizqitsani@gmail.com>
2022-11-28 20:39:04 +0700

    feat: add auth middleware

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,8 +1,14 @@
 import * as trpcNext from '@trpc/server/adapters/next';
 
+import { createContext } from '@/server/context';
 import { appRouter } from '@/server/routers/_app';
 
+/**
+ * Create tRPC's HTTP response handler
+ *
+ * @see https://github.com/trpc/examples-next-prisma-starter/blob/main/src/pages/api/trpc/%5Btrpc%5D.ts
+ */
 export default trpcNext.createNextApiHandler({
   router: appRouter,
-  createContext: () => ({}),
+  createContext,
 });
--- src/server/trpc.ts
+++ src/server/trpc.ts
@@ -1,6 +1,25 @@
-import { initTRPC } from '@trpc/server';
+import { initTRPC, TRPCError } from '@trpc/server';
 
-const t = initTRPC.create();
+import { Context } from '@/server/context';
+
+const t = initTRPC.context<Context>().create();
+
+const isAuthenticated = t.middleware(({ ctx, next }) => {
+  if (!ctx.session?.user) {
+    throw new TRPCError({
+      code: 'UNAUTHORIZED',
+      message: 'You must be logged in to access this resource.',
+    });
+  }
+
+  return next({
+    ctx: {
+      user: ctx.session.user,
+    },
+  });
+});
 
 export const router = t.router;
+
 export const publicProcedure = t.procedure;
+export const protectedProcedure = t.procedure.use(isAuthenticated);
