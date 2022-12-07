734b24f03a66ab87cbfe185aab80da709de26a55
rizqitsani <rizqitsani@gmail.com>
2022-12-07 15:38:37 +0700

    feat: integrate create category

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -1,9 +1,11 @@
 import { accountRouter } from '@/server/routers/account';
+import { categoryRouter } from '@/server/routers/category';
 import { mockRouter } from '@/server/routers/mock';
 import { router } from '@/server/trpc';
 
 export const appRouter = router({
   account: accountRouter,
+  category: categoryRouter,
   mock: mockRouter,
 });
 
